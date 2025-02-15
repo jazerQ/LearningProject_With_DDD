775210c74c5e423549ca82369678c4f6440c454a
JazerQ <saltanhayrliev2006@gmail.com>
2025-02-15 03:17:16 +0300

    Third commit

--- Infrastructure/JwtProvider.cs
+++ Infrastructure/JwtProvider.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure
 {
-    internal class JwtProvider
+    public class JwtProvider
     {
+        public string GenerateToken(User user) 
+        {
+            Fi file = new File();
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey());
+
+            var token = new JwtSecurityToken()
+        }
     }
 }
