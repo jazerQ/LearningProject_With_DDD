39adc062e6e867ccc7d0219fd6b7706ed37a0ea9
JazerQ <saltanhayrliev2006@gmail.com>
2025-02-16 01:32:42 +0300

    correct Secretkey

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d6ae8a5bfd0cdb1f1c2d85127a141a9112cbd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71be2e7dcaeb9affb663936a1f28d9f0f19067e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
