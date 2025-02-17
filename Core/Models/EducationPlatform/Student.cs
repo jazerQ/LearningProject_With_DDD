a743c2b2c1f80c9fbfd3c650e616151c002ed6c7
JazerQ <saltanhayrliev2006@gmail.com>
2025-02-18 00:11:45 +0300

    add Student model

--- Core/Models/EducationPlatform/Student.cs
+++ Core/Models/EducationPlatform/Student.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
 
 namespace Core.Models.EducationPlatform
 {
-    internal class Student
+    public class Student
     {
+        public Student(int id, string username)
+        {
+            this.Id = id;
+            this.Username = username;
+        }
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        private readonly List<Course> _courses = new();
+        public IReadOnlyCollection<Course> Courses => _courses;
+        public void AddValue(List<Course> courses) => _courses.AddRange(courses);
+        private static readonly string pattern = "\b(nigger | jew | fag)\b";
+        public static Result<Student> Create(int id, string username) 
+        {
+            if (string.IsNullOrEmpty(username) || Regex.IsMatch(username, pattern, RegexOptions.IgnoreCase)) 
+            {
+                return Result.Failure<Student>("your username is empty or offensive");
+            }
+            Student student = new Student(id, username);
+            return Result.Success<Student>(student);
+        }
     }
 }
